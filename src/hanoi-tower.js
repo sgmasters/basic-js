45426bb6f94b132a02de61b3137841f62604791e
Zhenya Rusak <unsgmasters@gmail.com>
2020-03-21 16:06:35 +0200

    feat: tower hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,10 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let minCount = countHanoiMoves(disksNumber);
+    let minSeconds = (minCount / turnsSpeed) * 3600;
+    return {turns: minCount, seconds: minSeconds};
+
+    function countHanoiMoves(n) {
+        if (n == 0) return 0;
+        return 2 * countHanoiMoves(n - 1) + 1;
+    }
 }
