e5bf7630c8ad5c8b75652f3e29fd2475128bc02a
Zhenya Rusak <unsgmasters@gmail.com>
2020-03-21 15:39:11 +0200

    feat: solve what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,21 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date == null) {
+    return 'Unable to determine the time of year!'
+  }
+  if (!(date instanceof Date) || isNaN(date.valueOf())) {
+    throw 'Error';
+  }
+
+  if ([11, 0, 1].some((a) => {return a === date.getMonth()})) {
+    return 'winter';
+  }
+  if ([4, 3, 2].some((a) => {return a === date.getMonth()})) {
+    return 'spring';
+  }
+  if ([5, 6, 7].some((a) => {return a === date.getMonth()})) {
+    return 'summer';
+  }
+  if ([10, 9, 8].some((a) => {return a === date.getMonth()})) {
+    return 'autumn';
+  }
 };
