d0fc7251a244fcaf3316cfb01180434028925b83
Zhenya Rusak <unsgmasters@gmail.com>
2020-03-21 15:02:49 +0200

    feat: solve dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,24 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members == null) {
+    return false;
+  }
+  if (members.constructor !== Array) {
+    return false;
+  }
+  let letters = [];
+  for (let member of members) {
+    if (typeof member === 'string') {
+      letters.push(member.trim().slice(0,1).toUpperCase());
+    }
+  }
+  let team = letters.sort((a, b) => {
+    if(a < b) { return -1; }
+    if(a > b) { return 1; }
+    return 0;
+  });
+  let teamName = '';
+  for(let letter of team) {
+    teamName += letter;
+  }
+  return teamName;
 };
