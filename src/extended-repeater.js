63daca88302e14f6a26553190e734411371599c5
Zhenya Rusak <unsgmasters@gmail.com>
2020-03-21 19:42:15 +0200

    feat: implement extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,44 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let string = checkStringOrConvert(str);
+
+    let addition = checkStringOrConvert(options['addition']);
+    let separator = (options['separator'] == null)? '+' : options['separator'];
+    let additionSeparator = (options['additionSeparator'] == null)? '|' : options['additionSeparator'];
+    let repeatTimes = checkExistsOrSetOne(options['repeatTimes']);
+    let additionRepeatTimes = checkExistsOrSetOne(options['additionRepeatTimes']);
+
+    let resultStr = stringWithAddition(string, addition, additionSeparator, additionRepeatTimes);
+    for (let i = 0; i < repeatTimes-1; i++) {
+        resultStr += separator + stringWithAddition(string, addition, additionSeparator, additionRepeatTimes);
+    }
+
+    return resultStr;
+
+    function stringWithAddition(str, add, sep, repeat) {
+        if (add === 'undefined') {
+            return str;
+        }
+        let result = str + add;
+        for (let i = 0; i < repeat - 1; i++) {
+            result += sep + add;
+        }
+        return result;
+    }
+
+    function checkExistsOrSetOne(object) {
+        if (object == null) {
+            return 1;
+        } else {
+            return object;
+        }
+    }
+
+    function checkStringOrConvert(object) {
+        if (typeof object === 'string') {
+            return object;
+        } else {
+            return '' + object;
+        }
+    }
 };
   
